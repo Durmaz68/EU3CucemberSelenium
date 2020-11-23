c6a8ddbdb8c4a6429cae9e84842975ed0ce872f4
Durmaz68 <67482135+Durmaz68@users.noreply.github.com>
2020-11-23 19:44:09 +0000

    re-run added

--- src/test/resources/features/AccountTypes.feature
+++ src/test/resources/features/AccountTypes.feature
@@ -1,15 +1,15 @@
 Feature: Account Types
-
+  @wip
   Scenario:  Driver user
     Given the user logged as "driver"
     When the user navigate to "Activities" "Calendar Events"
     Then the title contains "Calendar Events - Activities"
-
+  @wip
   Scenario: Sales Manager user
     Given the user logged as "sales manager"
-    When  the user navigate to "Customer" "Accounts"
+    When  the user navigate to "Customers" "Accounts"
     Then the title contains "Accounts - Customers"
-
+  @wip
   Scenario: Store Manager user
     Given the user logged as "store manager"
     When the user navigate to "Customers" "Contacts"
@@ -24,7 +24,6 @@ Feature: Account Types
   #Scenario: Store Manger user
   #  Given the user logged as "store maneger"
 
-  @wip
   Scenario Outline: Login with diffirent accounts<usertype>
     Given the user logged as "<userType>"
     When the user navigate to "<tab>" "<module>"
--- target/default-html-reports/report.js
+++ target/default-html-reports/report.js
@@ -4,71 +4,10 @@ formatter.feature({
   "description": "",
   "keyword": "Feature"
 });
-formatter.scenarioOutline({
-  "name": "Login with diffirent accounts",
-  "description": "",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "name": "@wip"
-    }
-  ]
-});
-formatter.step({
-  "name": "the user logged as \"\u003cuserType\u003e\"",
-  "keyword": "Given "
-});
-formatter.step({
-  "name": "the user navigate to \"\u003ctab\u003e\" \"\u003cmodule\u003e\"",
-  "keyword": "When "
-});
-formatter.step({
-  "name": "the title contains \"\u003ctitle\u003e\"",
-  "keyword": "Then "
-});
-formatter.examples({
-  "name": "",
-  "description": "",
-  "keyword": "Examples",
-  "rows": [
-    {
-      "cells": [
-        "userType",
-        "tab",
-        "module",
-        "title"
-      ]
-    },
-    {
-      "cells": [
-        "driver",
-        "Activities",
-        "Calendar Events",
-        "Calendar Events - Activities"
-      ]
-    },
-    {
-      "cells": [
-        "store manager",
-        "Customers",
-        "Contacts",
-        "Contacts - Customers"
-      ]
-    },
-    {
-      "cells": [
-        "sales manager",
-        "Customers",
-        "Accounts",
-        "Accounts - Customers"
-      ]
-    }
-  ]
-});
 formatter.scenario({
-  "name": "Login with diffirent accounts",
+  "name": "Driver user",
   "description": "",
-  "keyword": "Scenario Outline",
+  "keyword": "Scenario",
   "tags": [
     {
       "name": "@wip"
@@ -112,9 +51,57 @@ formatter.after({
   "status": "passed"
 });
 formatter.scenario({
-  "name": "Login with diffirent accounts",
+  "name": "Sales Manager user",
   "description": "",
-  "keyword": "Scenario Outline",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@wip"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logged as \"sales manager\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.ContactsStepDefs.the_user_logged_as(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user navigate to \"Customers\" \"AccountS\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.NavigationMenuStepDefs.the_user_navigate_to(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//span[normalize-space()\u003d\u0027AccountS\u0027 and contains(@class, \u0027title title-level-2\u0027)]\"}\n  (Session info: chrome\u003d86.0.4240.198)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027MacBook-Pro.local\u0027, ip: \u0027127.0.0.1\u0027, os.name: \u0027Mac OS X\u0027, os.arch: \u0027x86_64\u0027, os.version: \u002710.15.7\u0027, java.version: \u00271.8.0_242\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 86.0.4240.198, chrome: {chromedriverVersion: 86.0.4240.22 (398b0743353ff..., userDataDir: /var/folders/jc/20r07q252mx...}, goog:chromeOptions: {debuggerAddress: localhost:50195}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: MAC, platformName: MAC, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 67ece725bf033a6eafb0f6e2f38ecd07\n*** Element info: {Using\u003dxpath, value\u003d//span[normalize-space()\u003d\u0027AccountS\u0027 and contains(@class, \u0027title title-level-2\u0027)]}\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\n\tat com.vytrack.pages.BasePage.navigateToModule(BasePage.java:114)\n\tat com.vytrack.step_definitions.NavigationMenuStepDefs.the_user_navigate_to(NavigationMenuStepDefs.java:46)\n\tat ✽.the user navigate to \"Customers\" \"AccountS\"(file:///Users/durmaz/IdeaProjects/EU3CucemberSelenium/src/test/resources/features/AccountTypes.feature:10)\n",
+  "status": "failed"
+});
+formatter.step({
+  "name": "the title contains \"Accounts - Customers\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.LoginStepDefs.the_title_contains(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.embedding("image/png", "embedded0.png", "screenshot");
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Store Manager user",
+  "description": "",
+  "keyword": "Scenario",
   "tags": [
     {
       "name": "@wip"
@@ -146,52 +133,6 @@ formatter.result({
 });
 formatter.step({
   "name": "the title contains \"Contacts - Customers\"",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "com.vytrack.step_definitions.LoginStepDefs.the_title_contains(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Login with diffirent accounts",
-  "description": "",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "name": "@wip"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the user logged as \"sales manager\"",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "com.vytrack.step_definitions.ContactsStepDefs.the_user_logged_as(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the user navigate to \"Customers\" \"Accounts\"",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "com.vytrack.step_definitions.NavigationMenuStepDefs.the_user_navigate_to(java.lang.String,java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the title contains \"Accounts - Customers\"",
   "keyword": "Then "
 });
 formatter.match({
